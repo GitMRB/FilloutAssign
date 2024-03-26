e741e17d614ec6c1199d74c2499f9d73dc7a39aa
Mitch Bessett <mrbessett@gmail.com>
2024-03-25 20:32:25 -0700

    Added types and typescript; removed console debug statements

--- index.ts
+++ index.ts
@@ -1,43 +1,50 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import express from "express";
+import { Request, Response, NextFunction } from 'express';
 import axios from "axios";
+
+type FilterClauseType = {
+	id: string;
+	condition: 'equals' | 'does_not_equal' | 'greater_than' | 'less_than';
+	value: number | string;
+}
+
+type ResponseFiltersType = FilterClauseType[];
+
 const app = express();
 app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('This is a GET request at /');
 });
-app.get('/:formId/filteredResponses', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+
+app.get('/:formId/filteredResponses', async (req, res) => {
     const { formId } = req.params;
     const { filters } = req.query;
-    let parsedFilters;
+    let parsedFilters: FilterClauseType[];
+
     if (!filters) {
         return res.status(400).json({ error: 'No filters were provided.' });
     }
+
     if (typeof filters === 'string') {
         parsedFilters = JSON.parse(filters);
-    }
-    else {
+    } else {
         throw Error;
     }
+
     try {
-        const response = yield axios.get(`https://api.fillout.com/v1/api/forms/${formId}/submissions`, {
+        const response = await axios.get(`https://api.fillout.com/v1/api/forms/${formId}/submissions`, {
             headers: {
                 'Authorization': `Bearer ${process.env.BEARER_TOKEN}`
             }
         });
-        const filteredResponses = response.data.responses.filter((response) => {
+
+        const filteredResponses: ResponseFiltersType = response.data.responses.filter((response: { questions: any[]; }) => {
             return parsedFilters.every(filter => {
-                const question = response.questions.find((q) => q.id === filter.id);
-                if (!question)
-                    return false;
+                const question = response.questions.find((q: { id: string; }) => q.id === filter.id);
+
+                if (!question) return false;
+
                 switch (filter.condition) {
                     case 'equals':
                         return question.value === filter.value;
@@ -52,19 +59,21 @@ app.get('/:formId/filteredResponses', (req, res) => __awaiter(void 0, void 0, vo
                 }
             });
         });
+
         res.json({
             responses: filteredResponses,
             totalResponses: filteredResponses.length,
             pageCount: Math.ceil(filteredResponses.length / 10)
         });
-    }
-    catch (error) {
+    } catch (error: any) {
         res.status(500).json({ error: error.toString() });
     }
-}));
-app.use((err, req, res, next) => {
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
     res.status(500).send("Something is broken!");
-});
+})
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
